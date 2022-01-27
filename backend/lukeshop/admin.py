72ab4e5dc560ec4cb53c52279b47f14687e74510
lukew007 <75794966+lukew007@users.noreply.github.com>
2022-01-27 14:35:03 +0000

    Django setup complete.

--- backend/lukeshop/admin.py
+++ backend/lukeshop/admin.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import APIUser
+from .models import *
 
-admin.site.register(APIUser, UserAdmin)
+admin.site.register(APIUser)
+admin.site.register(Product)
+admin.site.register(Basket)
+admin.site.register(BasketItem)
+admin.site.register(Order)
